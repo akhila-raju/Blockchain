a8cba05aa3b727e83d272df90d91b43d35ebbee6
Akhila Raju <akhilaraju@berkeley.edu>
2015-10-18 17:17:11 -0700

    basis for part 1

--- balances.c
+++ balances.c
@@ -84,11 +84,66 @@ int main(int argc, char *argv[])
 		}
 
 		/* TODO */
-		/* Feel free to add/modify/delete any code you need to. */
+		/* Check the singleton conditions */
+
+		// Compute hash
+		bool valid = true;
+		hash_output curr_hash;
+		curr_hash = block_hash(b, h);
+
+		// If the block has height 0 (the “genesis block”), its SHA256 hash must be the hardcoded value 0000000e5ac98c789800702ad2a6f3ca510d409d6cca892ed1c75198e04bdeec. (Use the byte32_cmp function.)
+		if (b->height == 0) {
+			if (byte32_cmp(curr_hash, GENESIS_BLOCK_HASH) == 1) { 
+				valid = valid & true; // block is valid
+			} else {  
+				valid = valid & false; // block is invalid
+			}
+		}
+
+		// • The hash of the block must be smaller than TARGET_HASH; i.e., it must start with 24 zero bits. (Use the hash_output_is_below_target function.)
+		if (hash_output_is_below_target(h) == 1) {
+			valid = valid & true; // block is valid
+		} else {
+			valid = valid & false; // block is invalid
+		}
+
+		// • The height of both of the block's transactions must be equal to the block's height.
+		if (b->reward_tx.height == b->height && b->normal_tx.height == b->height) {
+			valid = valid & true; // block is valid
+		} else {
+			valid = valid & false; // block is invalid			
+		}
+
+		// add to list of valid blocks
+		if (valid) {
+			// add to list of valid blocks
+		}
 	}
 
 	/* Organize into a tree, check validity, and output balances. */
 	/* TODO */
+
+	// sort list of valid blocks
+
+	for (i = 1; i < argc; i++) {
+		char *filename;
+		struct block b;
+		int rc;
+
+		filename = argv[i];
+		rc = block_read_filename(&b, filename);
+		if (rc != 1) {
+			fprintf(stderr, "could not read %s\n", filename);
+			exit(1);
+		}
+
+		if 
+
+	}
+
+// validity
+// If a block has height ≥1, its parent must be a valid block with a height that is 1 smaller.
+// reward_tx.prev_transaction_hash, reward_tx.src_signature.r, and reward_tx.src_signature.s members must be zero—reward transactions are not signed and do not come from another public key. (Use the byte32_zero function.)
 
 	struct balance *balances = NULL, *p, *next;
 	/* Print out the list of balances. */
