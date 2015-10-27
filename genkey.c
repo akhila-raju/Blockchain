ca000853492fd8852ddf9e8bbdea3830a4f2af40
Patrick Jones <patrick_j_jones@sbcglobal.net>
2015-10-27 14:35:18 -0700

    Update genkey.c

--- genkey.c
+++ genkey.c
@@ -74,7 +74,7 @@ int main(int argc, char *argv[])
 
 	filename = argv[1];
 
-	// key = generate_key(); // THIS GENERATES A SECURE KEY!!!!!!
+	key = generate_key(); // THIS GENERATES A SECURE KEY!!!!!!
 	// unsigned char buf[32];
 	// int i;
 	// srand(1234);
@@ -83,20 +83,20 @@ int main(int argc, char *argv[])
 	// }
 	// key = generate_key_from_buffer(buf);
 	// ABOVE GENERATES WEAK KEY!!!!
-	time_t = 1443700800-1000;
-	while(KEYNOTEQUAL) { //CORRECT THIS AS SOON AS I CAN?
-		time_t = 1443700800 + 1;
-		unsigned char buf[32];
-		int i;
-		srand(cur_time);
-		for (i = 0; i < 32; i++) {
-			buf[i] = rand() & 0xff;
-		}
-		if (key == NULL) {
-			fprintf(stderr, "error generating key\n");
-			exit(1);
-		}
-	}
+	// time_t cur_time = 1443700800-1000;
+	// while(KEYNOTEQUAL) { //CORRECT THIS AS SOON AS I CAN?
+	// 	cur_time = 1443700800 + 1;
+	// 	unsigned char buf[32];
+	// 	int i;
+	// 	srand(cur_time);
+	// 	for (i = 0; i < 32; i++) {
+	// 		buf[i] = rand() & 0xff;
+	// 	}
+	// 	if (key == NULL) {
+	// 		fprintf(stderr, "error generating key\n");
+	// 		exit(1);
+	// 	}
+	// }
 	rc = key_write_filename(filename, key);
 	if (rc != 1) {
 		fprintf(stderr, "error saving key\n");
